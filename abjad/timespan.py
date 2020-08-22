375b5232f81bd230d581b4f3f3c3b31dadc9bca3
Trevor Bača <trevor.baca@gmail.com>
2020-08-22 14:46:09 -0400

    Cleaned up timespan markup.

--- abjad/timespan.py
+++ abjad/timespan.py
@@ -3051,7 +3051,8 @@ class TimespanList(TypedList):
                 >>> lilypond_file = abjad.illustrate(timespans)
                 >>> markup = lilypond_file.items[-1]
                 >>> abjad.f(markup)
-                \markup \column { \overlay
+                \markup \column {
+                \overlay
                     {
                         \translate
                             #'(1.0 . 1)
@@ -3116,85 +3117,83 @@ class TimespanList(TypedList):
                                         \fraction
                                             16
                                             1
-                    } \pad-to-box
-                    #'(0 . 82.33333333333333)
-                    #'(0 . 8.5)
-                    \postscript
-                        #"
-                        0.2 setlinewidth
-                        1 6.5 moveto
-                        67.66666666666667 6.5 lineto
-                        stroke
-                        1 7.25 moveto
-                        1 5.75 lineto
-                        stroke
-                        67.66666666666667 7.25 moveto
-                        67.66666666666667 5.75 lineto
-                        stroke
-                        17.666666666666668 3.5 moveto
-                        67.66666666666667 3.5 lineto
-                        stroke
-                        17.666666666666668 4.25 moveto
-                        17.666666666666668 2.75 lineto
-                        stroke
-                        67.66666666666667 4.25 moveto
-                        67.66666666666667 2.75 lineto
-                        stroke
-                        101.00000000000001 3.5 moveto
-                        151 3.5 lineto
-                        stroke
-                        101.00000000000001 4.25 moveto
-                        101.00000000000001 2.75 lineto
-                        stroke
-                        151 4.25 moveto
-                        151 2.75 lineto
-                        stroke
-                        59.33333333333334 0.5 moveto
-                        67.66666666666667 0.5 lineto
-                        stroke
-                        59.33333333333334 1.25 moveto
-                        59.33333333333334 -0.25 lineto
-                        stroke
-                        67.66666666666667 1.25 moveto
-                        67.66666666666667 -0.25 lineto
-                        stroke
-                        101.00000000000001 0.5 moveto
-                        117.66666666666667 0.5 lineto
-                        stroke
-                        101.00000000000001 1.25 moveto
-                        101.00000000000001 -0.25 lineto
-                        stroke
-                        117.66666666666667 1.25 moveto
-                        117.66666666666667 -0.25 lineto
-                        stroke
-                        0.1 setlinewidth
-                        [ 0.1 0.2 ] 0 setdash
-                        1 8.5 moveto
-                        1 7 lineto
-                        stroke
-                        17.666666666666668 8.5 moveto
-                        17.666666666666668 4 lineto
-                        stroke
-                        59.33333333333334 8.5 moveto
-                        59.33333333333334 1 lineto
-                        stroke
-                        67.66666666666667 8.5 moveto
-                        67.66666666666667 1 lineto
-                        stroke
-                        101.00000000000001 8.5 moveto
-                        101.00000000000001 1 lineto
-                        stroke
-                        117.66666666666667 8.5 moveto
-                        117.66666666666667 1 lineto
-                        stroke
-                        151 8.5 moveto
-                        151 4 lineto
-                        stroke
-                        0 0 moveto
-                        0.99 setgray
-                        0 0.01 rlineto
-                        stroke
-                        " }
+                    }
+                \pad-to-box #'(0 . 82.33333333333333) #'(0 . 8.5)
+                \postscript #"
+                0.2 setlinewidth
+                1 6.5 moveto
+                67.66666666666667 6.5 lineto
+                stroke
+                1 7.25 moveto
+                1 5.75 lineto
+                stroke
+                67.66666666666667 7.25 moveto
+                67.66666666666667 5.75 lineto
+                stroke
+                17.666666666666668 3.5 moveto
+                67.66666666666667 3.5 lineto
+                stroke
+                17.666666666666668 4.25 moveto
+                17.666666666666668 2.75 lineto
+                stroke
+                67.66666666666667 4.25 moveto
+                67.66666666666667 2.75 lineto
+                stroke
+                101.00000000000001 3.5 moveto
+                151 3.5 lineto
+                stroke
+                101.00000000000001 4.25 moveto
+                101.00000000000001 2.75 lineto
+                stroke
+                151 4.25 moveto
+                151 2.75 lineto
+                stroke
+                59.33333333333334 0.5 moveto
+                67.66666666666667 0.5 lineto
+                stroke
+                59.33333333333334 1.25 moveto
+                59.33333333333334 -0.25 lineto
+                stroke
+                67.66666666666667 1.25 moveto
+                67.66666666666667 -0.25 lineto
+                stroke
+                101.00000000000001 0.5 moveto
+                117.66666666666667 0.5 lineto
+                stroke
+                101.00000000000001 1.25 moveto
+                101.00000000000001 -0.25 lineto
+                stroke
+                117.66666666666667 1.25 moveto
+                117.66666666666667 -0.25 lineto
+                stroke
+                0.1 setlinewidth
+                [ 0.1 0.2 ] 0 setdash
+                1 8.5 moveto
+                1 7 lineto
+                stroke
+                17.666666666666668 8.5 moveto
+                17.666666666666668 4 lineto
+                stroke
+                59.33333333333334 8.5 moveto
+                59.33333333333334 1 lineto
+                stroke
+                67.66666666666667 8.5 moveto
+                67.66666666666667 1 lineto
+                stroke
+                101.00000000000001 8.5 moveto
+                101.00000000000001 1 lineto
+                stroke
+                117.66666666666667 8.5 moveto
+                117.66666666666667 1 lineto
+                stroke
+                151 8.5 moveto
+                151 4 lineto
+                stroke
+                0 0 moveto
+                0.99 setgray
+                0 0.01 rlineto
+                stroke"
+                }
 
         ..  container:: example
 
@@ -3236,7 +3235,8 @@ class TimespanList(TypedList):
                 \left-column {
                 \fontsize #-1 \sans \line { "voice 1:" }
                 \vspace #0.5
-                \column { \overlay
+                \column {
+                \overlay
                     {
                         \translate
                             #'(1.0 . 1)
@@ -3256,38 +3256,37 @@ class TimespanList(TypedList):
                                         \fraction
                                             1
                                             4
-                    } \pad-to-box
-                    #'(0 . 149.0)
-                    #'(0 . 2.5)
-                    \postscript
-                        #"
-                        0.2 setlinewidth
-                        1 0.5 moveto
-                        151 0.5 lineto
-                        stroke
-                        1 1.25 moveto
-                        1 -0.25 lineto
-                        stroke
-                        151 1.25 moveto
-                        151 -0.25 lineto
-                        stroke
-                        0.1 setlinewidth
-                        [ 0.1 0.2 ] 0 setdash
-                        1 2.5 moveto
-                        1 1 lineto
-                        stroke
-                        151 2.5 moveto
-                        151 1 lineto
-                        stroke
-                        0 0 moveto
-                        0.99 setgray
-                        0 0.01 rlineto
-                        stroke
-                        " }
+                    }
+                \pad-to-box #'(0 . 149.0) #'(0 . 2.5)
+                \postscript #"
+                0.2 setlinewidth
+                1 0.5 moveto
+                151 0.5 lineto
+                stroke
+                1 1.25 moveto
+                1 -0.25 lineto
+                stroke
+                151 1.25 moveto
+                151 -0.25 lineto
+                stroke
+                0.1 setlinewidth
+                [ 0.1 0.2 ] 0 setdash
+                1 2.5 moveto
+                1 1 lineto
+                stroke
+                151 2.5 moveto
+                151 1 lineto
+                stroke
+                0 0 moveto
+                0.99 setgray
+                0 0.01 rlineto
+                stroke"
+                }
                 \vspace #0.5
                 \fontsize #-1 \sans \line { "voice 2:" }
                 \vspace #0.5
-                \column { \overlay
+                \column {
+                \overlay
                     {
                         \translate
                             #'(1.0 . 1)
@@ -3307,38 +3306,37 @@ class TimespanList(TypedList):
                                         \fraction
                                             1
                                             4
-                    } \pad-to-box
-                    #'(0 . 149.0)
-                    #'(0 . 2.5)
-                    \postscript
-                        #"
-                        0.2 setlinewidth
-                        1 0.5 moveto
-                        151 0.5 lineto
-                        stroke
-                        1 1.25 moveto
-                        1 -0.25 lineto
-                        stroke
-                        151 1.25 moveto
-                        151 -0.25 lineto
-                        stroke
-                        0.1 setlinewidth
-                        [ 0.1 0.2 ] 0 setdash
-                        1 2.5 moveto
-                        1 1 lineto
-                        stroke
-                        151 2.5 moveto
-                        151 1 lineto
-                        stroke
-                        0 0 moveto
-                        0.99 setgray
-                        0 0.01 rlineto
-                        stroke
-                        " }
+                    }
+                \pad-to-box #'(0 . 149.0) #'(0 . 2.5)
+                \postscript #"
+                0.2 setlinewidth
+                1 0.5 moveto
+                151 0.5 lineto
+                stroke
+                1 1.25 moveto
+                1 -0.25 lineto
+                stroke
+                151 1.25 moveto
+                151 -0.25 lineto
+                stroke
+                0.1 setlinewidth
+                [ 0.1 0.2 ] 0 setdash
+                1 2.5 moveto
+                1 1 lineto
+                stroke
+                151 2.5 moveto
+                151 1 lineto
+                stroke
+                0 0 moveto
+                0.99 setgray
+                0 0.01 rlineto
+                stroke"
+                }
                 \vspace #0.5
                 \fontsize #-1 \sans \line { "voice 10:" }
                 \vspace #0.5
-                \column { \overlay
+                \column {
+                \overlay
                     {
                         \translate
                             #'(1.0 . 1)
@@ -3358,34 +3356,32 @@ class TimespanList(TypedList):
                                         \fraction
                                             1
                                             4
-                    } \pad-to-box
-                    #'(0 . 149.0)
-                    #'(0 . 2.5)
-                    \postscript
-                        #"
-                        0.2 setlinewidth
-                        1 0.5 moveto
-                        151 0.5 lineto
-                        stroke
-                        1 1.25 moveto
-                        1 -0.25 lineto
-                        stroke
-                        151 1.25 moveto
-                        151 -0.25 lineto
-                        stroke
-                        0.1 setlinewidth
-                        [ 0.1 0.2 ] 0 setdash
-                        1 2.5 moveto
-                        1 1 lineto
-                        stroke
-                        151 2.5 moveto
-                        151 1 lineto
-                        stroke
-                        0 0 moveto
-                        0.99 setgray
-                        0 0.01 rlineto
-                        stroke
-                        " }
+                    }
+                \pad-to-box #'(0 . 149.0) #'(0 . 2.5)
+                \postscript #"
+                0.2 setlinewidth
+                1 0.5 moveto
+                151 0.5 lineto
+                stroke
+                1 1.25 moveto
+                1 -0.25 lineto
+                stroke
+                151 1.25 moveto
+                151 -0.25 lineto
+                stroke
+                0.1 setlinewidth
+                [ 0.1 0.2 ] 0 setdash
+                1 2.5 moveto
+                1 1 lineto
+                stroke
+                151 2.5 moveto
+                151 1 lineto
+                stroke
+                0 0 moveto
+                0.99 setgray
+                0 0.01 rlineto
+                stroke"
+                }
                 }
 
         Returns markup.
@@ -3621,9 +3617,8 @@ class TimespanList(TypedList):
         x_extent *= postscript_scale
         x_extent += postscript_x_offset
         y_extent = height + 1.5
-        string = rf"\pad-to-box #'(0 . {x_extent}) #'(0 . {y_extent})"
-        string += rf' \postscript #"{ps}"'
-        lines_markup = Markup(string)
+        lines_string = rf"\pad-to-box #'(0 . {x_extent}) #'(0 . {y_extent})"
+        lines_string += f'\n\\postscript #"\n{ps}"'
         fraction_markups = []
         for offset in sorted(offset_mapping):
             offset = Multiplier(offset)
@@ -3637,8 +3632,7 @@ class TimespanList(TypedList):
         fraction_markup = Markup.overlay(fraction_markups)
         assert len(fraction_markup.contents) == 1, repr(fraction_markup)
         fraction_ = fraction_markup.contents[0]
-        assert len(lines_markup.contents) == 1, repr(lines_markup)
-        string = rf"\column {{ {fraction_} {lines_markup.contents[0]} }}"
+        string = f"\\column {{\n{fraction_}\n{lines_string}\n}}"
         return string
 
     ### PUBLIC PROPERTIES ###
